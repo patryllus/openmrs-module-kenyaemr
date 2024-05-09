98a0a00c0c27fc56c6c7dd907fa72a4c36515e33
patryllus <gpatryllus@gmail.com>
2024-05-09 17:05:44 +0300

    Added remarks and corrected art start date in referrals and linkage register

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/HTSLinkageArtStartDateDataEvaluator.java
@@ -0,0 +1,49 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr.reporting.data.converter.definition.evaluator;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.HTSLinkageArtStartDateToDataDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.HTSLinkageProviderHandedToDataDefinition;
+import org.openmrs.module.reporting.data.encounter.EvaluatedEncounterData;
+import org.openmrs.module.reporting.data.encounter.definition.EncounterDataDefinition;
+import org.openmrs.module.reporting.data.encounter.evaluator.EncounterDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * Evaluates a VisitIdDataDefinition to produce a VisitData
+ */
+@Handler(supports= HTSLinkageArtStartDateToDataDefinition.class, order=50)
+public class HTSLinkageArtStartDateDataEvaluator implements EncounterDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedEncounterData evaluate(EncounterDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        EvaluatedEncounterData c = new EvaluatedEncounterData(definition, context);
+
+        String qry = "select rl.encounter_id, coalesce(date(rl.art_start_date),date(dr.date_started))\n" +
+			"from kenyaemr_etl.etl_hts_referral_and_linkage rl\n" +
+			"         left join (select dr.patient_id, min(dr.date_started) as date_started from kenyaemr_etl.etl_drug_event dr where dr.program = 'HIV' group by dr.patient_id) dr\n" +
+			"        on dr.patient_id = rl.patient_id;";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        queryBuilder.append(qry);
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+}
