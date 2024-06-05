cdc8076aa6312c9b3b7cb643eedc2b7effe0a538
patryllus <gpatryllus@gmail.com>
2024-06-05 14:59:19 +0300

    Aligned GBV concepts to CHEB Obunge ETLed concepts - program, encounter and forms

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/GBVMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/GBVMetadata.java
@@ -25,37 +25,43 @@ import static org.openmrs.module.metadatadeploy.bundle.CoreConstructors.*;
 public class GBVMetadata extends AbstractMetadataBundle {
 
 	public static final class _Program {
-		public static final String GBV = Metadata.Program.GBV;
+
+		public static final String GBV_TREATMENT = "e41c3d74-37c7-4001-9f19-ef9e35224b70";
+	}
+
+	public static final class _Concept {
+
+		public static final String GBV_TREATMENT = "124824AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 	}
 	public static final class _EncounterType {
-		public static final String GBV_ENROLLMENT_ENCOUNTER = "998a55ca-f5af-461b-b4ec-3346220f0bcc";
-		public static final String GBV_TRAUMA_COUNSELLING_ENCOUNTER = "61a5d2aa-1d34-4c84-9eb1-2714d7bd0d22";
-		public static final String GBV_DISCONTINUATION_ENCOUNTER = "e6924edb-b5b5-441d-a2dc-fa940243bd66";
-		public static final String GBV_COMMUNITY_LINKAGE_ENCOUNTER = "19a77c1b-efba-4070-af2b-5e21037b31bf";
-		public static final String GBV_LEGAL_ENCOUNTER = "3c92b068-b362-4ae1-900d-bf26cca45d67";
-		public static final String GBV_PERPETRATOR_DETAILS_ENCOUNTER = "1a290c2e-cb1c-4736-acdf-c12f809a0462";
-		public static final String GBV_CONSENT_ENCOUNTER = "2715d976-f4a1-4fb1-adb4-accca540d610";
-		public static final String PEP_MANAGEMENT_NON_OCN_ENCOUNTER = "6dd20d4e-d7c5-4c04-b648-373d2619b5be";
-		public static final String PEP_MANAGEMENT_OCN_ENCOUNTER = "2feae60e-2cb8-47fc-83dc-24ccff32eb06";
-		public static final String PEP_MANAGEMENT_SURVIVOR_ENCOUNTER = "7fee62dc-6252-40d4-b216-f459ac5bd54c";
-		public static final String SGBV_POST_RAPE_CARE_PART_A_ENCOUNTER = "8396c582-7242-4eb7-a580-15fc0c3951ea";
-		public static final String SGBV_PSYCHOLOGICAL_ASSESSMENT_PART_B_ENCOUNTER = "d459d923-4d6a-426e-a4ae-871608e12c5b";
+		public static final String GBV_ENROLLMENT_ENCOUNTER = "b3f8c498-7f17-44c7-993e-6cd981a5f420";
+		public static final String GBV_TRAUMA_COUNSELLING_ENCOUNTER = "16048e65-24c6-4833-a342-29000143298f";
+		public static final String GBV_DISCONTINUATION_ENCOUNTER = "ab3031fe-d67c-4ae4-9317-f6f5b920b776";
+		public static final String GBV_COMMUNITY_LINKAGE_ENCOUNTER = "b38c73ee-6949-4f5f-8013-a89e36474d72";
+		public static final String GBV_LEGAL_ENCOUNTER = "8941dc56-afda-4155-866f-e9fc0e71f5fd";
+		public static final String GBV_PERPETRATOR_DETAILS_ENCOUNTER = "189e128f-3212-47ca-9d89-ee1876c394a9";
+		public static final String GBV_CONSENT_ENCOUNTER = "3809b3fc-bcdd-46fe-a461-60a341a842d0";
+		public static final String PEP_MANAGEMENT_NON_OCN_ENCOUNTER = "4f718f68-b414-4e27-803e-b4fbbc959d89";
+		public static final String PEP_MANAGEMENT_OCN_ENCOUNTER = "5c1ecaf1-ec25-46b7-9b5e-ee7fe44f03cf";
+		public static final String PEP_MANAGEMENT_SURVIVOR_ENCOUNTER = "133c8398-1fdc-437a-a74a-e73b1254c1d6";
+		public static final String SGBV_POST_RAPE_CARE_PART_A_ENCOUNTER = "e571c807-8fcc-4bc3-bc64-4ed372b348e4";
+		public static final String SGBV_PSYCHOLOGICAL_ASSESSMENT_PART_B_ENCOUNTER = "b5ab0a6b-9425-44da-b0d9-242f609f1605";
 
 	}
 
 	public static final class _Form {
-		public static final String GBV_ENROLLMENT_FORM = "461ca30e-a7f4-4dd9-9a7d-b6da63b92bdd";
-		public static final String GBV_TRAUMA_COUNSELLING_FORM = "48c995d4-200d-4259-b2f7-d3b1b13f8c77";
-		public static final String GBV_DISCONTINUATION_FORM = "4e563850-9c17-48fb-908e-5bbdce85f900";
-		public static final String GBV_COMMUNITY_LINKAGE_FORM = "f3d7f7b9-af4d-4545-9834-102905fb8790";
-		public static final String GBV_LEGAL_FORM = "60fa054e-7144-4671-8111-555f669ab285";
-		public static final String GBV_PERPETRATOR_DETAILS_FORM = "c8add03a-8b3f-4e8e-9265-a55809af676e";
-		public static final String GBV_CONSENT_FORM = "b3c4e545-b9b3-42f6-a41c-9758679af178";
-		public static final String PEP_MANAGEMENT_NON_OCN_FORM = "70d46a54-d07c-4658-8ccd-feba6bc941ec";
-		public static final String PEP_MANAGEMENT_OCN_FORM = "9f9dacfa-7952-4f9f-b191-1ff557ae2be4";
-		public static final String PEP_MANAGEMENT_SURVIVOR_FORM = "f6539ca2-fa98-4007-8945-85cbf7d19cc3";
-		public static final String SGBV_POST_RAPE_CARE_PART_A_FORM = "c3101e2e-ac1e-4b18-8ad1-0fe8a514cf8a";
-		public static final String SGBV_PSYCHOLOGICAL_ASSESSMENT_PART_B_FORM = "c766da7b-efa3-45e5-b090-1ed3b4339cab";
+		public static final String GBV_ENROLLMENT_FORM = "9ba1d4aa-57d7-48f9-a635-a23508e8136c";
+		public static final String GBV_TRAUMA_COUNSELLING_FORM = "e983d758-5adf-4917-8172-0f4be4d8116a";
+		public static final String GBV_DISCONTINUATION_FORM = "8fed3d06-f8a1-4cb8-b853-cd93394bab79";
+		public static final String GBV_COMMUNITY_LINKAGE_FORM = "f760e38c-3d2f-4a5d-aa3d-e9682576efa8";
+		public static final String GBV_LEGAL_FORM = "d0c36426-4503-4236-ab5d-39bff77f2b50";
+		public static final String GBV_PERPETRATOR_DETAILS_FORM = "f37d7e0e-95e8-430d-96a3-8e22664f74d6";
+		public static final String GBV_CONSENT_FORM = "d720a8b3-52cc-41e2-9a75-3fd0d67744e5";
+		public static final String PEP_MANAGEMENT_NON_OCN_FORM = "92de9269-6bb4-4c24-8ec9-870aa2c64b5a";
+		public static final String PEP_MANAGEMENT_OCN_FORM = "556718da-7564-4286-a081-c50f8e4b6504";
+		public static final String PEP_MANAGEMENT_SURVIVOR_FORM = "f44b2405-226b-47c4-b98f-b826ea4725ae";
+		public static final String SGBV_POST_RAPE_CARE_PART_A_FORM = "c46aa4fd-8a5a-4675-90a7-a6f2119f61d8";
+		public static final String SGBV_PSYCHOLOGICAL_ASSESSMENT_PART_B_FORM = "9d21275a-7657-433a-b305-a736423cc496";
 	}
 	/**
 	 * @see AbstractMetadataBundle#install()
@@ -63,7 +69,8 @@ public class GBVMetadata extends AbstractMetadataBundle {
 	@Override
 	public void install() {
 
-		install(program("GBV", "Gender Based Violence Program", Dictionary.GBV_PROGRAM, _Program.GBV));
+		install(program("GBV", "GBV Treatment Program", _Concept.GBV_TREATMENT,
+			_Program.GBV_TREATMENT));
 		install(encounterType("GBV Enrollment Encounter", "GBV Enrollment Encounter", _EncounterType.GBV_ENROLLMENT_ENCOUNTER));
 		install(encounterType("GBV Trauma Counselling Encounter", "GBV Trauma Counselling Encounter", _EncounterType.GBV_TRAUMA_COUNSELLING_ENCOUNTER));
 		install(encounterType("GBV Discontinuation Encounter", "GBV Discontinuation Encounter", _EncounterType.GBV_DISCONTINUATION_ENCOUNTER));
