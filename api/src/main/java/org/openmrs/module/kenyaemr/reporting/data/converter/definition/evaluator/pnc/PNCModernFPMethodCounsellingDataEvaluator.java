c6f1ff046cc6ef3b81036282bd5a31084df40e4f
patryllus <gpatryllus@gmail.com>
2024-06-27 12:58:10 +0300

    PNC register: Add multiple fp methods

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/pnc/PNCModernFPMethodCounsellingDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/pnc/PNCModernFPMethodCounsellingDataEvaluator.java
@@ -36,20 +36,9 @@ public class PNCModernFPMethodCounsellingDataEvaluator implements EncounterDataE
         EvaluatedEncounterData c = new EvaluatedEncounterData(definition, context);
 
         String qry = "select v.encounter_id,\n" +
-			"       (case v.family_planning_method when 160570 then 'Emergency contraceptive pills'\n" +
-			"                                    when 780 then 'Oral Contraceptives Pills' \n" +
-			"                                    when 5279 then 'Injectible' \n" +
-			"                                    when 1359 then 'Implant'\n" +
-			"                                    when 5275 then 'Intrauterine Device' \n" +
-			"                                    when 136163 then 'Lactational Amenorhea Method' \n" +
-			"                                    when 5278 then 'Diaphram/Cervical Cap'\n" +
-			"                                    when 5277 then 'Fertility Awareness'\n" +
-			"                                    when 1472 then 'Tubal Ligation' \n" +
-			"                                    when 190 then 'Condoms' \n" +
-			"                                    when 1489 then 'Vasectomy' \n" +
-			"                                    when 162332 then 'Undecided' else '' end) as family_planning_method\n" +
-			"    from kenyaemr_etl.etl_mch_postnatal_visit v\n" +
-			"    where date(v.visit_date) between date(:startDate) and date(:endDate);";
+			"    v.family_planning_method as family_planning_method\n" +
+			"from kenyaemr_etl.etl_mch_postnatal_visit v\n" +
+			"where date(v.visit_date) between date(:startDate) and date(:endDate);";
 
         SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
         queryBuilder.append(qry);
