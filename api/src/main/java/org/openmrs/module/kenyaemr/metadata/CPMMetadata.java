37ca62783c9511329e8776b63e5578232a996964
patryllus <gpatryllus@gmail.com>
2024-07-01 16:39:29 +0300

    Added CPM screening form for Community Pharmacy Module

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/CPMMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/CPMMetadata.java
@@ -31,6 +31,7 @@ public class CPMMetadata extends AbstractMetadataBundle {
 		public static final String CPM_ENROLLMENT_ENCOUNTER = "e0700664-90b6-4480-aad2-6e0a00babd66";
 		public static final String CPM_REFERRAL_ENCOUNTER = "c49952bf-218b-44b4-8d2a-1947cbf00fff";
 		public static final String CPM_DISCONTINUATION_ENCOUNTER = "ddc73ecc-ad85-40bc-ad43-318ed9abfd00";
+		public static final String CPM_SCREENING_ENCOUNTER = "f0b27e6c-57cd-4dec-ad6b-43eee6e571ee";
 
 	}
 
@@ -38,6 +39,7 @@ public class CPMMetadata extends AbstractMetadataBundle {
 		public static final String CPM_ENROLLMENT_FORM = "f01c67f7-2293-4a6a-b0f6-5db0fb5934dd";
 		public static final String CPM_REFERRAL_FORM = "b1e1f6fe-1894-4d06-bd8c-2b87f7cd9577";
 		public static final String CPM_DISCONTINUATION_FORM = "d1e7ebb4-afc0-412b-a98b-6720ab1169cc";
+		public static final String CPM_SCREENING_FORM = "f7dbe6b6-2a5a-46e6-af52-45bf2962f4aa";
 	}
 	/**
 	 * @see AbstractMetadataBundle#install()
@@ -49,10 +51,12 @@ public class CPMMetadata extends AbstractMetadataBundle {
 		install(encounterType("CPM Enrollment Encounter", "CPM Enrollment Encounter", _EncounterType.CPM_ENROLLMENT_ENCOUNTER));
 		install(encounterType("CPM Referral Encounter", "CPM Referral Encounter", _EncounterType.CPM_REFERRAL_ENCOUNTER));
 		install(encounterType("CPM Discontinuation Encounter", "CPM Discontinuation Encounter", _EncounterType.CPM_DISCONTINUATION_ENCOUNTER));		
+		install(encounterType("CPM Screening Encounter", "CPM Screening Encounter", _EncounterType.CPM_SCREENING_ENCOUNTER));		
 
 		install(form("CPM Enrollment Form", "Community Pharmacy Model Enrollment form", _EncounterType.CPM_ENROLLMENT_ENCOUNTER, "1", _Form.CPM_ENROLLMENT_FORM));
 		install(form("CPM Referral Form", "Community Pharmacy Model Referral form", _EncounterType.CPM_REFERRAL_ENCOUNTER, "1", _Form.CPM_REFERRAL_FORM));
 		install(form("CPM Discontinuation Form", "Community Pharmacy Model Discontinuation  Form", _EncounterType.CPM_DISCONTINUATION_ENCOUNTER, "1", _Form.CPM_DISCONTINUATION_FORM));
+		install(form("CPM Screening Form", "Community Pharmacy Model Screening  Form", _EncounterType.CPM_SCREENING_ENCOUNTER, "1", _Form.CPM_SCREENING_FORM));
 	
 	}
 }
