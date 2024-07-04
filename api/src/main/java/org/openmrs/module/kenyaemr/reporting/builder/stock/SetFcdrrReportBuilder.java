516808091b8050ab7d8199b7cd722dc57cf5d633
patryllus <gpatryllus@gmail.com>
2024-07-04 12:14:24 +0300

    Initial stubs for FCDRR - updated template and Dataset definitions

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/stock/SetFcdrrReportBuilder.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/stock/SetFcdrrReportBuilder.java
@@ -28,24 +28,83 @@ import java.util.List;
 @Builds({ "kenyaemr.ehrReports.report.fcdrr" })
 public class SetFcdrrReportBuilder extends AbstractReportBuilder {
 
-    @Override
-    protected List<Parameter> getParameters(ReportDescriptor reportDescriptor) {
-        return Arrays.asList(new Parameter("startDate", "Start Date", Date.class), new Parameter("endDate", "End Date",
-                Date.class), new Parameter("dateBasedReporting", "", String.class));
-    }
+	//FCDRR is meant only for drugs, so we use drug_ID for uniqueness
+	static final int ABACAVIR_300MG_TABS = 1384;
+	static final int ABACAVIR_LAMIVUDINE_600MG_300MG_TABS = 4873;
+	static final int ATANAZAVIR_RITONAVIR_300_100MG_TABS = 122;
+	static final int DARUNAVIR_600MG = 2221;
+	static final int DOLUTEGRAVIR_50MG_TABS  = 2225;
 
-    @Override
-    protected List<Mapped<DataSetDefinition>> buildDataSets(ReportDescriptor reportDescriptor,
-                                                            ReportDefinition reportDefinition) {
-        return Arrays.asList(ReportUtils.map(getFcdrrDatasetDefition(), "startDate=${startDate},endDate=${endDate}"));
-    }
+	@Override
+	protected List<Parameter> getParameters(ReportDescriptor reportDescriptor) {
+		return Arrays.asList(new Parameter("startDate", "Start Date", Date.class), new Parameter("endDate", "End Date",
+			Date.class), new Parameter("dateBasedReporting", "", String.class));
+	}
 
-    private DataSetDefinition getFcdrrDatasetDefition(){
-        SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
-        sqlDataSetDefinition.setName("FCDRR");
-        sqlDataSetDefinition.addParameter(new Parameter("startDate", "Start Date", Date.class));
-        sqlDataSetDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
-        sqlDataSetDefinition.setSqlQuery("SELECT COUNT(*) AS total_items FROM stockmgmt_stock_item WHERE date_created BETWEEN :startDate AND :endDate ");
-        return sqlDataSetDefinition;
-    }
+	@Override
+	protected List<Mapped<DataSetDefinition>> buildDataSets(ReportDescriptor reportDescriptor,
+															ReportDefinition reportDefinition) {
+		return Arrays.asList(ReportUtils.map(getFcdrrABCDatasetDefition(), "startDate=${startDate},endDate=${endDate}"),
+			ReportUtils.map(getFcdrrABC3TCDatasetDefition(),"startDate=${startDate},endDate=${endDate}"));
+	}
+
+	private DataSetDefinition getFcdrrABCDatasetDefition() {
+		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("ABC");
+		sqlDataSetDefinition.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		sqlDataSetDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		sqlDataSetDefinition.setSqlQuery(getFcdrrDrugSummary(ABACAVIR_300MG_TABS));
+		return sqlDataSetDefinition;
+	}
+	private DataSetDefinition getFcdrrABC3TCDatasetDefition() {
+		SqlDataSetDefinition sqlDataSetDefinition = new SqlDataSetDefinition();
+		sqlDataSetDefinition.setName("ABC3TC");
+		sqlDataSetDefinition.addParameter(new Parameter("startDate", "Start Date", Date.class));
+		sqlDataSetDefinition.addParameter(new Parameter("endDate", "End Date", Date.class));
+		sqlDataSetDefinition.setSqlQuery(getFcdrrDrugSummary(ABACAVIR_LAMIVUDINE_600MG_300MG_TABS));
+		return sqlDataSetDefinition;
+	}
+
+
+
+	private String getFcdrrDrugSummary(int drugId) {
+
+		String query =	"select                     sspu.factor as unit_pack_size,\n" +
+			"    SUM(stit.quantity) + rc.quantity - si.quantity as opening_balance,\n" +
+			"                                     rc_curr.quantity as curr_receipts\n" +
+			"      from stockmgmt_stock_item_transaction stit\n" +
+			"               inner join stockmgmt_stock_item ssi on ssi.stock_item_id = stit.stock_item_id and ssi.drug_id = %d\n" +
+			"               inner join stockmgmt_stock_item_packaging_uom sspu on sspu.stock_item_id = stit.stock_item_id\n" +
+			"               inner join stockmgmt_stock_operation ssto on stit.stock_operation_id = ssto.operation_type_id and ssto.status = 'COMPLETED'\n" +
+			"                          and ssto.operation_type_id in (4, 9) and stit.date_created between date_sub(date(:startDate) , interval 1 MONTH) and date_sub(date(:endDate) , interval 1 MONTH)\n" +
+			"         left join (\n" +
+			"    select SUM(stit.quantity) as quantity, stit.stock_item_id\n" +
+			"    from stockmgmt_stock_item_transaction stit\n" +
+			"             inner join stockmgmt_stock_item ssi on ssi.stock_item_id = stit.stock_item_id\n" +
+			"             inner join stockmgmt_stock_operation ssto on stit.stock_operation_id = ssto.operation_type_id\n" +
+			"    WHERE ssi.drug_id = %d\n" +
+			"      AND ssto.status = 'COMPLETED'\n" +
+			"      AND stit.date_created between date_sub(date(:startDate) , interval 1 MONTH) and date_sub(date(:endDate) , interval 1 MONTH)\n" +
+			"      AND ssto.operation_type_id in (4)) rc on rc.stock_item_id = stit.stock_item_id\n" +
+			"         left join (\n" +
+			"    select SUM(stit.quantity) as quantity, stit.stock_item_id\n" +
+			"    from stockmgmt_stock_item_transaction stit\n" +
+			"             inner join stockmgmt_stock_item ssi on ssi.stock_item_id = stit.stock_item_id\n" +
+			"             inner join stockmgmt_stock_operation ssto on stit.stock_operation_id = ssto.operation_type_id\n" +
+			"    WHERE ssi.drug_id = %d\n" +
+			"      AND ssto.status = 'COMPLETED'\n" +
+			"      AND stit.date_created between date_sub(date(:startDate) , interval 1 MONTH) and date_sub(date(:endDate) , interval 1 MONTH)\n" +
+			"      AND ssto.operation_type_id in (6, 3, 2)) si on si.stock_item_id = stit.stock_item_id\n" +
+			"               left join (\n" +
+			"          select SUM(stit.quantity) as quantity, stit.stock_item_id\n" +
+			"          from stockmgmt_stock_item_transaction stit\n" +
+			"                   inner join stockmgmt_stock_item ssi on ssi.stock_item_id = stit.stock_item_id\n" +
+			"                   inner join stockmgmt_stock_operation ssto on stit.stock_operation_id = ssto.operation_type_id\n" +
+			"          WHERE ssi.drug_id = %d\n" +
+			"            AND ssto.status = 'COMPLETED'\n" +
+			"            AND stit.date_created between :startDate and :endDate\n" +
+			"            AND ssto.operation_type_id in (4)) rc_curr on rc_curr.stock_item_id = stit.stock_item_id\n";
+
+		return String.format(query, drugId, drugId, drugId, drugId);
+	}
 }
